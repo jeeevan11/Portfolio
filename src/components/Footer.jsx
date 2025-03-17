d85352ac710d6102adfaefeb87044d32abd03344
Jatin Chhanwal <connectwithjatin365@gmail.com>
2025-03-17 13:30:22 +0530

    Initial commit — Vite+React scaffold, project list

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+function Footer(){
+    return(
+        <>
+            <div id="footerDiv">
+                <p id="hire">Available for Hire!</p>
+                <p id="footer">2025</p>
+            </div>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/Switch.jsx
@@ -0,0 +1,10 @@
+import './Switch.css'
+
+function Switch(){
+    return(
+        <div id="switchBtn">
+        </div>
+    )
+}
+
+export default Switch;
